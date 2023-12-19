1038ffad26e6433148fcfe583bde28310fa7f860
juliusw <uimmv@student.kit.edu>
2023-12-19 11:15:00 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="bad-phylo",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     entry_points={
         'console_scripts': ['bad = app.__main__:main']
